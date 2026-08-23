eea983a2ad3d95ef7701e6d9e8666ec904f9ced3
Abduletif Abdella <abduletifabdella29@gmail.com>
2026-08-23 11:40:59 +0300

    feat: add responsive header and scroll effect Added responsive header layouts for different screen sizes. Improved navigation, search, profile, and overall header behavior across desktop, tablet, and mobile devices. Implemented scroll detection to change the header background when the user scrolls, creating a smooth scroll-based header background effect.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,83 +1,119 @@
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
 import logo from '../../assets/Images/logo.png';
-import {Link} from 'react-router-dom';
-import {Search, Bell, User, ChevronDown} from 'lucide-react';
-import styles from './Header.module.css'
+import { Link } from 'react-router-dom';
+import { Search, Bell, User, ChevronDown } from 'lucide-react';
+import styles from './Header.module.css';
 
 function Header() {
 
-    const [isSearchOpen, setSearchOpen] = useState(false)
-    const [isProfileOpen, setIsProfileOpen] = useState(false)
+    const [isSearchOpen, setSearchOpen] = useState(false);
+    const [isProfileOpen, setIsProfileOpen] = useState(false);
 
-return (
-<header className={styles.header}>
-    <div className={styles.container}>
-        {/* logo */}
-        <img className={styles.logo} src={logo} alt="" />
+    // for blur
+    const [isScrolled, setIsScrolled] = useState(false);
 
-        {/* nav links */}
-        <nav className={styles.nav}>
-            <Link className={styles.navLink} href="">Home</Link>
-            <Link className={styles.navLink} href="">Tv Shows</Link>
-            <Link className={styles.navLink} href="">Movies</Link>
-            <Link className={styles.navLink} href="">New & Popular</Link>
-            <Link className={styles.navLink} href="">My List</Link>
-            <Link className={styles.navLink} href="">Browse by Language</Link>
-        </nav>
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
 
-        
-        {/* right side section */}
-        <div className={styles.rightSection}>
-            {/* search */}
-            <div className={styles.SearchContainer}>
-                <button onClick={() => setSearchOpen(!isSearchOpen)}
-                className={styles.SearchButton}>
-                    <Search size={20}/>
-                </button>
+        window.addEventListener('scroll', handleScroll);
 
-                {
-                    isSearchOpen && (
-                        <input type="text" placeholder='Search movie title' className={styles.searchInput} />
-                    )
-                }
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
-            </div>
+    return (
+        <header className={`${styles.header} ${isScrolled ? styles.scrolled : ""}`}>
+            <div className={styles.container}>
 
-                {/* notification */}
-            <button className={styles.iconButton}>
-                {/* notification icon */}
-                <Bell size={20}/>
-                <span className={styles.notificationBadge}>3</span>
-            </button>
+                {/* logo */}
+                <img className={styles.logo} src={logo} alt="" />
 
-            {/* profile */}
-            <div className={styles.profileContainer}>
-                <button onClick={() => setIsProfileOpen(!isProfileOpen)}
-                className={styles.profilebutton}>
-                    <div className={styles.profileAvatar}>
-                        {/* user icon */}
-                        <User size={20}/>
+                {/* nav links */}
+                <nav className={styles.nav}>
+                    <Link className={styles.navLink} to="/">Home</Link>
+                    <Link className={styles.navLink} to="/tv-shows">TV Shows</Link>
+                    <Link className={styles.navLink} to="/movies">Movies</Link>
+                    <Link className={styles.navLink} to="/popular">New & Popular</Link>
+                    <Link className={styles.navLink} to="/my-list">My List</Link>
+                    <Link className={styles.navLink} to="/browse">Browse by Language</Link>
+                </nav>
+
+                {/* right side section */}
+                <div className={styles.rightSection}>
+
+                    {/* search */}
+                    <div className={styles.SearchContainer}>
+                        <button
+                            onClick={() => setSearchOpen(!isSearchOpen)}
+                            className={styles.SearchButton}
+                        >
+                            <Search size={20} />
+                        </button>
+
+                        {isSearchOpen && (
+                            <input
+                                type="text"
+                                placeholder="Search movie title"
+                                className={styles.searchInput}
+                            />
+                        )}
                     </div>
-                        {/* dropdown icon */}
-                        <ChevronDown size={20}/>
-                </button>
 
-                {
-                    isProfileOpen && (
-                        <div className={styles.profileMenu}>
-                            <Link className={styles.profileMenuItem}>Account</Link>
-                            <Link className={styles.profileMenuItem}>help Center</Link>
-                            <hr className={styles.profileMenuDivider} />
-                            <button className={styles.profileMenuItem}>sign out</button>
-                        </div>
-                    )
-                }
+                    {/* notification */}
+                    <button className={styles.iconButton}>
+                        <Bell size={20} />
+                        <span className={styles.notificationBadge}>3</span>
+                    </button>
 
+                    {/* profile */}
+                    <div className={styles.profileContainer}>
+                        <button
+                            onClick={() => setIsProfileOpen(!isProfileOpen)}
+                            className={styles.profilebutton}
+                        >
+                            <div className={styles.profileAvatar}>
+                                <User size={20} />
+                            </div>
+
+                            <ChevronDown size={20} />
+                        </button>
+
+                        {isProfileOpen && (
+                            <div className={styles.profileMenu}>
+                                <Link
+                                    className={styles.profileMenuItem}
+                                    to="/account"
+                                >
+                                    Account
+                                </Link>
+
+                                <Link
+                                    className={styles.profileMenuItem}
+                                    to="/help"
+                                >
+                                    Help Center
+                                </Link>
+
+                                <hr className={styles.profileMenuDivider} />
+
+                                <button className={styles.profileMenuItem}>
+                                    Sign Out
+                                </button>
+                            </div>
+                        )}
+                    </div>
+
+                </div>
             </div>
-        </div>
-    </div>
-</header>
-)
+        </header>
+    );
 }
 
-export default Header
+export default Header;
